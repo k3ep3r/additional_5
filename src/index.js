414459d97f36b84dcd86a5f688b425a5d87cf46a
k3ep3r <keeper3211@gmail.com>
2018-02-24 21:17:57 +0300

    All test passed, atom always change package-lock.json, dont read this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var array = {},
+        buffer = str,
+        undef;
+    for(var i = 0; i < bracketsConfig.length; i++) {
+        array[bracketsConfig[i][1]] = bracketsConfig[i].join('');
+    }
+
+    for(var j = 0; j < str.length; j++) {
+        if(array[str[j]] !== undef) {
+            buffer = buffer.replace(array[str[j]], '');
+       }
+    }
+    
+    if (buffer.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
